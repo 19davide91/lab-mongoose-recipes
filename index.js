339e8d1bc583feb07440444aa0c3ab49557bbcb5
Davide Loprewsti <davidelopresti0129@gmail.com>
2022-12-15 18:34:47 +0000

    Completed lab

--- index.js
+++ index.js
@@ -1,23 +1,78 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Bucatini all' amatriciana",
+      level: "Easy Peasy",
+      ingredients: [
+        "1 tablespoon extra virgin olive oil",
+        "4-6 ounces pancetta (Italian bacon)",
+        "1 small yellow onion (roughly 6-7 ounces)",
+        "1 teaspoon crushed red pepper flakes",
+        "kosher salt",
+        "3 garlic cloves",
+        "2 tablespoons tomato paste",
+        "1 (28-ounce) can crushed San Marzano or Italian plum tomatoes",
+        "freshly ground black pepper",
+        "1 lb (16 ounces) rigatoni, bucatini, or spaghetti",
+        "1 ounce finely grated pecorino romano cheese",
+      ],
+      cuisine: "Italian",
+      dishType: "main_course",
+      duration: 20,
+      creator: "God",
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((newRecipe) => console.log("My recipe is", newRecipe.title))
+
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+
+  .then((result) => {
+    result.forEach((recipe) => console.log(`Name of recipe ${recipe.title}`));
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" }, { new: true });
+  })
+
+  .then(() => {
+    console.log("the deleted was succesfull!");
+  })
+
+  .then(() => {
+    mongoose.connection.close(() => {
+      console.log(
+        "Mongoose default connection disconnected through app termination"
+      );
+    });
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: {
+    type: [String],
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: {
+    type: String,
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
+  },
+  image: {
+    type: String,
+    default:
+      "https://www.tavolartegusto.it/wp/wp-content/uploads/2022/04/Amatriciana.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  creator: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: 15 / 12 / 2022,
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
